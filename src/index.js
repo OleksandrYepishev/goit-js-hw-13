3edb404535ebf689c320c1510d7c35e1ee7aa6cd
Oleksandr Yepishev <Alexandrdymka@gmail.com>
2021-07-24 23:44:51 +0300

    pagination

--- src/index.js
+++ src/index.js
@@ -3,28 +3,41 @@ import fetchPhotos from './js/fetchPhotos';
 import Notiflix from "notiflix";
 import galleryCard from './templates/galleryCard.hbs';
 
+let page = 1;
+const per_page = 4;
+let searchQuery = [];
+
 
 const fetchPhotosInput = document.getElementById('search-form');
 const galleryContainer = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
+
 
 fetchPhotosInput.addEventListener('submit', onSearch);
+loadMoreBtn.addEventListener('click', nextPage);
 
 async function onSearch(e) {
+    loadMoreBtn.disabled = false;
+    resetPage()
     e.preventDefault();
+
     const form = e.currentTarget;
-    const searchQuery = form.elements.searchQuery.value;
+    searchQuery = form.elements.searchQuery.value;
  
     try {
         clearGalleryMarkup()
-        const photos = await fetchPhotos(searchQuery);
+        const photos = await fetchPhotos(searchQuery, page, per_page);
         let totalHits = photos.data.totalHits;
         
-            if (totalHits === 0 || searchQuery.trim() === '' ) {
+        if (totalHits === 0 || searchQuery.trim() === '') {
+            loadMoreBtn.disabled = true;
                 let message = 'Sorry, there are no images matching your search query. Please try again.';
                return Notiflix.Notify.failure(`${message}`)
             }
-        Notiflix.Notify.info(`Hooray! We found ${totalHits} images.`)   
-        renderPhotos(photos)
+        Notiflix.Notify.info(`Hooray! We found ${totalHits} images.`);
+        renderPhotos(photos);
+        
+       
         
             } catch (error) {
         console.log(error)
@@ -32,12 +45,29 @@ async function onSearch(e) {
     
 };
 
+async function nextPage() {
+    const photos = await fetchPhotos(searchQuery, page, per_page);
+    renderPhotos(photos);
+};
+
 function renderPhotos(photos) {
     galleryContainer.insertAdjacentHTML('beforeend', galleryCard(photos));
-    
+    incrPage();
+    if (page > 1) {
+        loadMoreBtn.classList.remove('is-hidden');
+    }
+   
 };
 
 function clearGalleryMarkup() {
     galleryContainer.innerHTML = '';
    
-}
+};
+
+function incrPage() {
+    page += 1;
+};
+ 
+function resetPage(){
+    page = 1;
+ };
--- src/js/fetchPhotos.js
+++ src/js/fetchPhotos.js
@@ -4,9 +4,11 @@ const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '22089613-2c269b20f41d8e495fd067b59';
 const OPTIONS = 'image_type=photo&orientation=horizontal&safesearch=true';
 
-async function fetchPhotos(searchField) {
+
+async function fetchPhotos(searchField, page, per_page) {
+    
     try {
-        const response = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${searchField}&${OPTIONS}`);
+        const response = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${searchField}&${OPTIONS}&page=${page}&per_page=${per_page}`);
         return response
     } catch (error) {
         console.log(error)
